f412a2144e0d4be21b57c9c1e07fa7189e7b876d
john kuras <jkuras@islandchristianacademy.com>
2019-05-11 11:13:39 +0800

    added script to download exe files

---
+++ CLIENT_DATA/wget-exe-files.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# download the exe files required by this script
+
+wget http://www.coderforlife.com/projects/win7boot/downloads/Win7BootUpdater2.exe
+chmod ug+x Win7BootUpdater2.exe
+
+wget http://www.coderforlife.com/projects/win7boot/downloads/Win7BootUpdaterCmd2.exe
+chmod ug+x Win7BootUpdaterCmd2.exe
+
